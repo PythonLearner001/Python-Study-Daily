25647fad9ba18152c79294e1fd37ecfe91098962
PythonLearner001 <3021935505@qq.com>
2026-01-31 17:35:13 +0800

    Create 01_if循环.py

---
+++ Python_learning/循环语句/01_if循环.py
@@ -0,0 +1,25 @@
+"""
+age = 20
+if age >= 18:
+    print(f"年龄{age}你是成年人")
+"""
+"""
+score = 75
+if score >= 60:
+    print(f"分数{score}考试及格啦")
+else:
+    print(f"分数{score}考试不及格")
+"""
+"""
+score3 = 88
+if score3 >= 90:
+    print(f"分数{score3}成绩等级：优")
+elif score3 >= 80:  
+    print(f"分数{score3}成绩等级：良")
+elif score3 >= 70:  
+    print(f"分数{score3}成绩等级：中")
+elif score3 >= 60:  
+    print(f"分数{score3}成绩等级：及格")
+else: 
+    print(f"分数{score3}成绩等级：差")
+"""
